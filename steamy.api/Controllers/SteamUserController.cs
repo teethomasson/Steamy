26b12f00e54a54581d190554b7d0f8ff24049a54
teethomasson <tee.thomasson@gmail.com>
2023-04-30 18:02:49 -0400

    Added endpoints to API

--- steamy.api/Controllers/SteamUserController.cs
+++ steamy.api/Controllers/SteamUserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Steam.Models.SteamCommunity;
 
 namespace steamy.api.Controllers
 {
@@ -10,26 +11,26 @@ namespace steamy.api.Controllers
     [Route("[controller]")]
 public class SteamUserController : ControllerBase
 {
-        private readonly SteamService _steamService;
+    private readonly SteamService _steamService;
 
-        public SteamUserController(SteamService steamService)
-        {
-            _steamService = steamService;
-        }
+    public SteamUserController(SteamService steamService)
+    {
+        _steamService = steamService;
+    }
 
-        [HttpGet("vanity/{vanityUrl}")]
-        public async Task<ActionResult<ulong>> ResolveVanityUrlAsync(string vanityUrl)
+    [HttpGet("vanity/{vanityUrl}")]
+    public async Task<ActionResult<ulong>> ResolveVanityUrlAsync(string vanityUrl)
+    {
+        try
         {
-            try
-            {
-                ulong steamId = await _steamService.ResolveVanityUrlAsync(vanityUrl);
-                return Ok(steamId);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
+            ulong steamId = await _steamService.ResolveVanityUrlAsync(vanityUrl);
+            return Ok(steamId);
         }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
 
         [HttpGet("profile/{steamIdOrVanityUrl}")]
     public async Task<IActionResult> GetUserProfile(string steamIdOrVanityUrl)
@@ -50,8 +51,58 @@ public class SteamUserController : ControllerBase
             return BadRequest(new { message = ex.Message });
         }
     }
-    
+
+    public async Task<ActionResult<IEnumerable<OwnedGameModel>>> GetTopGamesByPlaytime(ulong steamId, int count  =15)
+    {
+        try
+        {
+            var topGames = await _steamService.GetTopGamesByPlaytimeAsync(steamId, count);
+            return Ok(topGames);
+        }
+            catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    public async Task<ActionResult<IEnumerable<RecentlyPlayedGameModel>>> GetRecentlyPlayedGames(ulong steamId)
+    {
+        try
+        {
+            var recentGames = await _steamService.GetRecentlyPlayedGames(steamId);
+            return Ok(recentGames);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    public async Task<ActionResult<IEnumerable<OwnedGameModel>>> GetAllOwnedGames(ulong steamId)
+    {
+        try
+        {
+            var ownedGames =  await _steamService.GetAllOwnedGames(steamId);
+            return Ok(ownedGames);  
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    public async Task<ActionResult<int>> GetSteamLevel(ulong steamId)
+    {
+        try
+        {
+            var steamLevel = await _steamService.GetSteamLevel(steamId);
+            return Ok(steamLevel);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
 }
 }
-
-
