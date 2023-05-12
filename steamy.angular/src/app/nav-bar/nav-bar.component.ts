577b9f6c205cb2cee4331718ce4a124ea4a23f4c
teethomasson <tee.thomasson@gmail.com>
2023-05-11 22:13:41 -0400

    nav bar changes, more to do

--- steamy.angular/src/app/nav-bar/nav-bar.component.ts
+++ steamy.angular/src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-  constructor(public dialog: MatDialog, private router: Router) {}
+export class NavBarComponent implements OnInit, OnDestroy {
+  isLoggedIn = false;
+  private authSubscription: Subscription | undefined;
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authSubscription = this.authService.isLoggedIn.subscribe(
+      (loggedIn) => {
+        this.isLoggedIn = loggedIn;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
 
   openSignUpDialog(): void {
     this.dialog.open(SignUpComponent, {
