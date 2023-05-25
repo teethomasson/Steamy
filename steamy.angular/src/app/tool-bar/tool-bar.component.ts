630d9acd655481c28ec081c18f37e7cf0b75ed77
teethomasson <tee.thomasson@gmail.com>
2023-05-24 21:37:54 -0400

    canvas implented. tooling bar outline

---
+++ steamy.angular/src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tool-bar',
+  templateUrl: './tool-bar.component.html',
+  styleUrls: ['./tool-bar.component.css']
+})
+export class ToolBarComponent {
+  constructor()
+}
