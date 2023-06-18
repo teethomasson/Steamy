3611ce2cf6d7117f2f102c960609b345004fa24b
teethomasson <tee.thomasson@gmail.com>
2023-06-18 08:32:55 -0400

    note canvas

--- steamy.angular/src/app/note-editor/note-editor.component.ts
+++ steamy.angular/src/app/note-editor/note-editor.component.ts
@@ -42,6 +42,7 @@ export class NoteEditorComponent implements OnInit {
       this.canvas.on('mouse:down', (options) => this.onMouseDown(options));
       this.canvas.on('mouse:move', (options) => this.onMouseMove(options));
       this.canvas.on('mouse:up', (options) => this.onMouseUp(options));
+      this.canvas.on('object:selected', (options) => this.onObjectSelected(options));
     });
   }
 
@@ -53,8 +54,9 @@ export class NoteEditorComponent implements OnInit {
         top: pointer.y,
         fill: 'transparent',
         stroke: 'black',
-        strokeWidth: 5,
+        strokeWidth: 1,
         selectable: false,
+        
       });
       this.canvas.add(this.tempRect);
     }
@@ -64,7 +66,7 @@ export class NoteEditorComponent implements OnInit {
     if (this.isTextboxMode && this.tempRect) {
       const pointer = this.canvas.getPointer(event.e);
       console.log('Pointer:', pointer); // Log the pointer values
-    console.log('TempRect:', this.tempRect); // Log the tempRect values
+      console.log('TempRect:', this.tempRect); // Log the tempRect values
       this.tempRect!.width = pointer.x - (this.tempRect!.left || 0);
       this.tempRect!.height = pointer.y - (this.tempRect!.top || 0);
       this.tempRect!.setCoords();
@@ -73,22 +75,61 @@ export class NoteEditorComponent implements OnInit {
   }
   
   
-  
-  
   onMouseUp(event: fabric.IEvent): void {
     if (this.isTextboxMode && this.tempRect) {
-      // Create the textbox at the position and size of the temporary rectangle
-      const textbox = new fabric.Textbox('', {
-        left: this.tempRect.left,
-        top: this.tempRect.top,
-        width: this.tempRect.width,
-        height: this.tempRect.height,
+        const textbox = new fabric.IText('', {
+            left: this.tempRect.left,
+            top: this.tempRect.top,
+            width: this.tempRect.width,
+            height: this.tempRect.height,
+            fontSize: 16,
+            borderColor: 'black',
+            borderScaleFactor: 1,
+            hasBorders: true,
+            editable: false,
+            lockMovementX: true,
+            lockMovementY: true,
+            lockScalingX: true,
+            lockScalingY: true,
+            lockRotation: true,
+            padding: 10,
+        });
+
+        this.canvas.add(textbox);
+        this.canvas.remove(this.tempRect);
+        this.tempRect = null;
+
+        this.canvas.on('mouse:up', (options) => {
+          if (options.target && options.target.type === 'i-text') {
+              let textbox = options.target as fabric.IText;
+              textbox.set({
+                  borderColor: 'red',
+              });
+              textbox.enterEditing();
+              this.canvas.renderAll();
+          }
       });
-      this.canvas.add(textbox);
-      this.canvas.remove(this.tempRect);
-      this.tempRect = null;
     }
+}
+
+
+
+onObjectSelected(event: fabric.IEvent): void {
+  const activeObject = this.canvas.getActiveObject();
+
+  if (activeObject && activeObject.type === 'textbox') {
+    const textbox = activeObject as fabric.Textbox;
+
+    textbox.set({
+      editable: true, // Make textbox editable on selection
+    });
+    textbox.enterEditing();
+    this.canvas.renderAll();
   }
+}
+
+
+
   
   
 
